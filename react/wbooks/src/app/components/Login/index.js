04ff99c5a51683162fee89c2622f0f1e3d3ec7d3
raparicio6 <rodrigoaparicio6@gmail.com>
2019-11-13 01:14:13 -0300

    redirecting to home after login

--- react/wbooks/src/app/components/Login/index.js
+++ react/wbooks/src/app/components/Login/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { set } from 'local-storage';
 import { object } from 'prop-types';
+import { withRouter } from 'react-router-dom';
 
 import imagePlaceholder from '../../assets/wolox-logo.png';
 import InputLabel from '../InputLabel';
@@ -36,6 +37,16 @@ class Login extends Component {
     const { errorMessages, isError } = this.state;
     const formattedErrorMessages = isArray(errorMessages) ? errorMessages : [errorMessages];
 
+    const LoginButton = withRouter(({ history }) => (
+      <button
+        type="button"
+        onClick={() => this.handleLogin(history)}
+        className={`${styles.loginButton} full-width m-top-4`}
+      >
+        {LOGIN}
+      </button>
+    ));
+
     return (
       <div className={`${styles.container} column background-wild-sand`}>
         <img src={imagePlaceholder} alt="Wolox logo" className={styles.woloxLogoImage} />
@@ -53,9 +64,7 @@ class Login extends Component {
               inputType={FIELDS[field].inputType}
             />
           ))}
-          <button type="submit" className={`${styles.loginButton} full-width m-top-4`}>
-            {LOGIN}
-          </button>
+          <LoginButton />
         </form>
         <Link className={`${styles.signUpButton} full-width`} to={Routes.SIGN_UP}>
           {SIGN_UP}
