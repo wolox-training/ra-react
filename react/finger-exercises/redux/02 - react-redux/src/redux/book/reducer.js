7f303d16291f3fa88ab2d5880b12f5936a039f61
raparicio6 <rodrigoaparicio6@gmail.com>
2020-01-09 00:16:49 -0300

    using destructuring in book reducer

--- react/finger-exercises/redux/02 - react-redux/src/redux/book/reducer.js
+++ react/finger-exercises/redux/02 - react-redux/src/redux/book/reducer.js
@@ -9,20 +9,33 @@ const initialState = {
 
 function reducer(state = initialState, action) {
   const { payload, type } = action;
+  let newBooksQuantity = {};
   switch (type) {
     case actions.GET_BOOKS:
       payload.forEach(book => {
-        if (!(book.id in state.booksQuantity)) state.booksQuantity = { ...state.booksQuantity, [book.id]: 1 };
+        if (!(book.id in state.booksQuantity)) {
+          newBooksQuantity = { ...newBooksQuantity, [book.id]: 1 };
+        }
       });
-      return { ...state, books: payload, originalData: payload };
+      return {
+        ...state,
+        books: payload,
+        originalData: payload,
+        booksQuantity: { ...state.booksQuantity, ...newBooksQuantity }
+      };
     case actions.ADD_TO_CART:
-      return { ...state, bookSelected: state.bookSelected.concat(payload) };
+      return { ...state, bookSelected: [...state.bookSelected, payload] };
     case actions.ADD_ITEM:
-      state.booksQuantity = { ...state.booksQuantity, [payload]: (state.booksQuantity[payload] += 1) };
-      return state;
+      return {
+        ...state,
+        booksQuantity: { ...state.booksQuantity, [payload]: (state.booksQuantity[payload] += 1) }
+      };
     case actions.REMOVE_ITEM:
-      state.booksQuantity = { ...state.booksQuantity, [payload]: 1 };
-      return { ...state, bookSelected: state.bookSelected.filter(book => book.id !== payload) };
+      return {
+        ...state,
+        booksQuantity: { ...state.booksQuantity, [payload]: 1 },
+        bookSelected: state.bookSelected.filter(book => book.id !== payload)
+      };
     case actions.SEARCH_ITEM:
       return {
         ...state,
