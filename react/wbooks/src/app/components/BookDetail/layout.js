f51e3ea4f6f4eaaa677a76388305ad0fcb15114d
raparicio6 <rodrigoaparicio6@gmail.com>
2020-01-20 23:53:57 -0300

    correct width in skeletons

--- react/wbooks/src/app/components/BookDetail/layout.js
+++ react/wbooks/src/app/components/BookDetail/layout.js
@@ -7,24 +7,19 @@ import bookCoverImage from '../../assets/book-cover.png';
 import badgeImage from '../../assets/badge.png';
 
 import styles from './styles.module.scss';
+import { SKELETONS_WIDTH } from './constants';
 
 function BookDetail({ title, genre, author, editorial, publicationYear, imageUrl, bookObtained }) {
   return (
     <div className={`column ${styles.container}`}>
       <div className={`${styles.bookInformationContent} row background-white`}>
         <div className="position-relative row center">
-          {bookObtained ? (
-            <>
-              <img
-                src={imageUrl || bookCoverImage}
-                alt="Wolox book cover"
-                className={styles.woloxBookCoverImage}
-              />
-              <img src={badgeImage} alt="Badge" className={styles.badge} />
-            </>
-          ) : (
-            <Skeleton />
-          )}
+          <img
+            src={imageUrl || bookCoverImage}
+            alt="Wolox book cover"
+            className={styles.woloxBookCoverImage}
+          />
+          <img src={badgeImage} alt="Badge" className={styles.badge} />
         </div>
         <div className={`column ${styles.bookDetails}`}>
           <h1 className={`${styles.title} bold}`}>
@@ -34,7 +29,7 @@ function BookDetail({ title, genre, author, editorial, publicationYear, imageUrl
                 <span className={`${styles.fieldDescription} bold ${styles.genre}`}>{`(${genre})`}</span>
               </>
             ) : (
-              <Skeleton />
+              <Skeleton width={`${SKELETONS_WIDTH.titleAndGenre}px`} />
             )}
           </h1>
           <p className={`${styles.field} bold`}>
@@ -44,7 +39,7 @@ function BookDetail({ title, genre, author, editorial, publicationYear, imageUrl
                 <span className={styles.fieldDescription}>{author}</span>
               </>
             ) : (
-              <Skeleton />
+              <Skeleton width={`${SKELETONS_WIDTH.fields}px`} />
             )}
           </p>
           <p className={`${styles.field} bold`}>
@@ -53,7 +48,7 @@ function BookDetail({ title, genre, author, editorial, publicationYear, imageUrl
                 {`${t('BookDetail:editorial')}:`} <span className={styles.fieldDescription}>{editorial}</span>
               </>
             ) : (
-              <Skeleton />
+              <Skeleton width={`${SKELETONS_WIDTH.fields}px`} />
             )}
           </p>
           <p className={`${styles.field} bold`}>
@@ -63,7 +58,7 @@ function BookDetail({ title, genre, author, editorial, publicationYear, imageUrl
                 <span className={styles.fieldDescription}>{publicationYear}</span>
               </>
             ) : (
-              <Skeleton />
+              <Skeleton width={`${SKELETONS_WIDTH.fields}px`} />
             )}
           </p>
         </div>
