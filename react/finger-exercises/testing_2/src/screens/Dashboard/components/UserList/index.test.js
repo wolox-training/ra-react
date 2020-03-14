380d33a66cdabae5fde76005ec3e6b3dd23907a4
raparicio6 <rodrigoaparicio6@gmail.com>
2020-03-14 03:15:22 -0300

    UserList tests

--- react/finger-exercises/testing_2/src/screens/Dashboard/components/UserList/index.test.js
+++ react/finger-exercises/testing_2/src/screens/Dashboard/components/UserList/index.test.js
@@ -4,13 +4,19 @@ import { configure, shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Adapter from 'enzyme-adapter-react-16';
 
+import UserRow from '../UserRow';
+
 import UserList from './index';
 
 configure({ adapter: new Adapter() });
 
 describe('UserList', () => {
-  xit('shows new user when props are changed', () => {
+  it('shows new user when props are changed', () => {
+    const users = [{ id:1, name: 'James'}, { id:2, name: 'Tom'}];
+    const wrapper = mount(<UserList users={users} />);
+    expect(wrapper.find(UserRow)).toHaveLength(2);
   });
+  // setProps is deprecated
   xit('setProps makes componentDidUpdate to be executed', () => {
   });
 });
